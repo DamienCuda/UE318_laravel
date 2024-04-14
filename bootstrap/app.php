6d2cadac9415cde1c0778d0eb1e968ecabecf219
Nils <nils.gill.arion@gmail.com>
2024-04-14 14:39:06 +0200

    Petite modification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,25 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Run The Shieldon Firewall
-|--------------------------------------------------------------------------
-|
-| Shieldon Firewall will watch all HTTP requests coming to your website.
-| Running Shieldon Firewall before initializing Laravel will avoid possible
-| conflicts with Laravel's built-in functions.
-*/
 if (isset($_SERVER['REQUEST_URI'])) {
 
-    // This directory must be writable.
-    // We put it in the `storage/shieldon_firewall` directory.
     $storage =  __DIR__ . '/../storage/shieldon_firewall';
 
     $firewall = new \Shieldon\Firewall\Firewall();
 
     $firewall->configure($storage);
 
-    // The base url for the control panel.
     $firewall->controlPanel('/firewall/panel/');
 
     $response = $firewall->run();
